2ea33bd9ae0bd5a3b65158e7f7b1d758cff79c8d
Sergio <s.benjamincodoceogomez@gmail.com>
2025-08-30 22:34:39 -0400

    uno de prueba xd

---
+++ proyectoSIA/src/main/java/com/mycompany/proyectosia/ProyectoSIA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectosia;
+
+/**
+ *
+ * @author benjo
+ */
+public class ProyectoSIA {
+
+    public static void main(String[] args) {
+        System.out.println("Esto es una prueba");
+    }
+}

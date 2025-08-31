3750b774884958ba35f31f29b4f1842e31e3591a
Sergio <s.benjamincodoceogomez@gmail.com>
2025-08-31 13:52:01 -0400

    Creacion clase recurso

--- proyectoSIA/src/main/java/com/mycompany/proyectosia/ProyectoSIA.java
+++ proyectoSIA/src/main/java/com/mycompany/proyectosia/ProyectoSIA.java
@@ -8,9 +8,19 @@ package com.mycompany.proyectosia;
  *
  * @author benjo
  */
+import java.util.*;
 public class ProyectoSIA {
-    public class estudiante{
+    public class usuario{
         
+    }
+    public class recurso{
+        private String id;
+        private String tipoUso;
+        private String titulo;
+        private String materia;
+        private boolean disponibilidad;
+        private int cupos;
+        private List<usuario> listaEspera;
     }
     public static void main(String[] args) {
         System.out.println("Esto es una prueba 2");
